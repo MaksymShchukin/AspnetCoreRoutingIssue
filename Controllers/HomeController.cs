4a64a7f9de8ae7e06e49e5010e7374d765008437
Maksym Shchukin <maxim.shchukin@gconnect.ch>
2020-03-06 14:50:15 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreRoutingIssue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("{a}.{b}.{c}/{d}")]
+        public IActionResult Test(string a, string b, string c, string d)
+        {
+            return Ok();
+        }
+    }
+}
